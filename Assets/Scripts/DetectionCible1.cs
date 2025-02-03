44755865014cc899ec25930c21a242e16b290e18
Eaulitviez <2077834@cegepmontpetit.ca>
2025-02-03 10:01:36 -0500

    script

--- Assets/Scripts/DetectionCible1.cs
+++ Assets/Scripts/DetectionCible1.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DetectionCible : MonoBehaviour
+public class DetectionCible1 : MonoBehaviour
 {
-    void OnCollisionEnter2D(Collision2D collision){
+    void OnCollisionEnter(Collision collision){
         print(collision.gameObject.name + " : " + collision.gameObject.tag);
         if(collision.gameObject.CompareTag("Cible")){
             Destroy(collision.gameObject);
